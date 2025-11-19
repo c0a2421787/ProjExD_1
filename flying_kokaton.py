8de67a1750beb0ee9ea3568350a1cd7002eae99e
C0A24217 <c0a2421787@edu.teu.ac.jp>
2025-11-19 21:15:20 +0900

    課題

---
+++ flying_kokaton.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False) # 練習8
+    kk_img = pg.transform.flip(pg.image.load("fig/3.png"), True, False) # 練習3
+    kk_rct=kk_img.get_rect() # 練習10
+    kk_rct.center=300, 200  # 練習10
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        x = tmr % 3200 # 練習5 -> 9
+        screen.blit(bg_img, [-x, 0]) # 練習5
+        screen.blit(bg_img2, [-x+1600, 0]) # 練習8
+        screen.blit(bg_img, [-x+3200, 0]) # 練習9
+        key_lst = pg.key.get_pressed()
+        dx = -1
+        dy = 0
+        if key_lst[pg.K_UP]:
+            dy -= 1
+        if key_lst[pg.K_DOWN]:
+            dy += 1
+        if key_lst[pg.K_LEFT]:
+            dx -= 1
+        if key_lst[pg.K_RIGHT]:
+            dx += 1
+        kk_rct.move_ip(dx, dy)
+        screen.blit(kk_img, kk_rct) # 練習4 -> 10
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200) # 練習6
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
